6af0abbf7d21f3b141f3e2ac229604f885af747d
AnaCarolinamoreiradesiqueira <carolina.trabalho94@gmail.com>
2025-05-26 22:36:21 -0300

    atualização com menu

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
-import Rotas from './components/Rotas';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// Importando suas telas já prontas
+import Home from './components/Home';
+import Sobre from './components/Sobre';
+
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Rotas />;
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Sobre" component={Sobre} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
